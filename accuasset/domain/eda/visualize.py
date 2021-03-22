68cd6295df0ab001cde708b3bcc264febc9ab0d6
hyeonsangjeon <wingnut0310@gmail.com>
2021-03-22 22:45:47 +0900

    s

--- accuasset/domain/eda/visualize.py
+++ accuasset/domain/eda/visualize.py
@@ -3,11 +3,17 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import os
+
 
 def test():
     print('\n', )
     print('domain > eda에 visualize입니다.')
 
+
+
+FILE = os.path.dirname(__file__)
+FONT_PATH = os.environ.get('FONT_PATH', os.path.join(FILE, 'NanumGothic.ttf'))
 
 def get_cloud(df, target_column, conditional_value='', conditional_column='all'):
 
@@ -21,7 +27,8 @@ def get_cloud(df, target_column, conditional_value='', conditional_column='all')
 
     plt.figure(figsize=(14,10)) #이미지 사이즈 지정
 
-    wordcloud = WordCloud(font_path='../../font/NanumGothic.ttf', background_color='white').generate_from_frequencies(words)
+    wordcloud = WordCloud(font_path=FONT_PATH, background_color='white').generate_from_frequencies(words)
+
     plt.imshow(wordcloud,interpolation='lanczos')
     plt.axis('off')
     #plt.set_title(str(target_column), fontsize=20)
--- setup.py
+++ setup.py
@@ -15,49 +15,6 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-
-class move_ttf(install):
-    def run(self):
-        """
-        Performs the usual install process and then copies the True Type fonts
-        that come with clearplot into matplotlib's True Type font directory,
-        and deletes the matplotlib fontList.cache
-        """
-        #Perform the usual install process
-        install.run(self)
-        #Try to install custom fonts
-        try:
-            import os, shutil
-            import matplotlib as mpl
-            import clearplot as cp
-
-            #Find where matplotlib stores its True Type fonts
-            mpl_data_dir = os.path.dirname(mpl.matplotlib_fname())
-            mpl_ttf_dir = os.path.join(mpl_data_dir, 'fonts', 'ttf')
-
-            #Copy the font files to matplotlib's True Type font directory
-            #(I originally tried to move the font files instead of copy them,
-            #but it did not seem to work, so I gave up.)
-            cp_ttf_dir = os.path.join(os.path.dirname(cp.__file__), 'true_type_fonts')
-            for file_name in os.listdir(cp_ttf_dir):
-                if file_name[-4:] == '.ttf':
-                    old_path = os.path.join(cp_ttf_dir, file_name)
-                    new_path = os.path.join(mpl_ttf_dir, file_name)
-                    shutil.copyfile(old_path, new_path)
-                    print ("Copying " + old_path + " -> " + new_path)
-
-            #Try to delete matplotlib's fontList cache
-            mpl_cache_dir = mpl.get_cachedir()
-            mpl_cache_dir_ls = os.listdir(mpl_cache_dir)
-            if 'fontList.cache' in mpl_cache_dir_ls:
-                fontList_path = os.path.join(mpl_cache_dir, 'fontList.cache')
-                os.remove(fontList_path)
-                print ("Deleted the matplotlib fontList.cache")
-        except:
-            warnings.warn("WARNING: An issue occured while installing the custom fonts for clearplot.")
-
-
-
 setuptools.setup(
     name="accuasset",
     version=prop['__version__'],
@@ -88,7 +45,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License"
     ],
     #package_data = {'' : ['color_maps/*.csv', 'font/*.ttf']},
-    package_data = {'' : ['accuasset/font/*.ttf']},
+    #package_data = {'' : ['accuasset/font/*.ttf']},
     #Specify the custom install class
-    cmdclass={'install' : move_ttf}
 )
