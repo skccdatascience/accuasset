990207bc5d2e47c4da69a70d4f67c79c225a7907
sooeun67 <32501921+sooeun67@users.noreply.github.com>
2021-03-26 12:47:01 +0900

    remove quick checks for understanding test data

--- test/general_function/test_domain_eda.py
+++ test/general_function/test_domain_eda.py
@@ -4,10 +4,6 @@ import seaborn as sns
 
 df = pd.read_csv('../test_data/dir_list.csv')
 
-# print(df.columns)
-# print(df.shape)
-# print(df.head(3))
-
 # accuasset.domain.eda.visualize.get_cloud(df,target_column='type')
 accuasset.domain.eda.visualize.test()
 
--- test/general_function/test_domain_nlp.py
+++ test/general_function/test_domain_nlp.py
@@ -3,9 +3,5 @@ import pandas as pd
 
 df = pd.read_csv('../test_data/dir_list.csv')
 
-# print(df.columns)
-# print(df.shape)
-# print(df.head(3))
-
 accuasset.domain.nlp.preprocessing.test()
 #accuasset.domain.nlp.preprocessing.preprocess_txt(df, 'type')
